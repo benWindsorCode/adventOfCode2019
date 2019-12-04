d41914ad31ec6803523c9dc2a3418d10a536271a
benWindsorCode <benjaminwindsor@hotmail.co.uk>
2019-12-04 22:12:28 +0000

    Fix minor day two bugs

--- src/DayTwo.hs
+++ src/DayTwo.hs
@@ -5,11 +5,11 @@ module DayTwo
 -- todo: do we want to represent program as a list of instructions?
 type Program = [Int]
 
-data Opcode = Add | Multiply | End deriving (Show)
+data Opcode = Add | Multiply | End deriving (Show, Eq)
 
 data Result = Result {
     output :: Int,
-    location :: Int
+    outputLocation :: Int
 }
 
 data Instruction = Instruction {
@@ -27,7 +27,7 @@ runProgramHelper :: Program -> Int -> Int -> Program
 runProgramHelper program pos 99 = program
 runProgramHelper program pos currentOpcode = runProgramHelper updatedProgram newPos (program !! newPos)
         where 
-            currentInstruction = parseInstruction [program !! pos, program !! (pos+1), program !! (pos+2), program !! (pos+3)]]
+            currentInstruction = parseInstruction [program !! pos, program !! (pos+1), program !! (pos+2), program !! (pos+3)]
             updatedProgram = updateProgram program (computeInstruction currentInstruction)
             newPos = pos+4
 
@@ -47,10 +47,10 @@ parseInstruction (opcode:rest)
 
 -- given a program, and the result of an instruction, return a new program with the value a the location updated
 updateProgram :: Program -> Result -> Program
-updateProgram program result = replaceNth program (location result) (output result)
+updateProgram program result = replaceNth program (outputLocation result) (output result)
 
 replaceNth :: [Int] -> Int -> Int -> [Int]
-replaceNth _ _ [] = []
+replaceNth [] _ _ = []
 replaceNth (x:xs) n newVal 
     | n == 0 = newVal:xs
-    | otherwise = x:replaceNth (n-1) newVal xs
+    | otherwise = x:replaceNth xs (n-1) newVal
