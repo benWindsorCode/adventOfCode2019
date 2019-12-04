529e787055c25138b41291d7e1421f1fe59e15ce
benWindsorCode <benjaminwindsor@hotmail.co.uk>
2019-12-04 18:55:09 +0000

    Continue implementation of day two

--- src/DayTwo.hs
+++ src/DayTwo.hs
@@ -7,12 +7,36 @@ type Program = [Int]
 
 data Opcode = Add | Multiply | End deriving (Show)
 
+data Result = Result {
+    output :: Int,
+    location :: Int
+}
+
 data Instruction = Instruction {
     opcode :: Opcode,
     firstVal :: Int,
     secondVal :: Int,
     location :: Int
 } deriving (Show)
+
+runProgram :: Program -> Program
+runProgram program = runProgramHelper program 0 (program !! 0)
+
+-- Given a program and current position and current opcode, then execute instruction and continue
+-- todo: implement exit condition
+runProgramHelper :: Program -> Int -> Int -> Program
+runProgramHelper program pos 99 = program
+runProgramHelper program pos currentOpcode = runProgramHelper updatedProgram newPos (program !! newPos)
+        where 
+            currentInstruction = parseInstruction [program !! pos, program !! (pos+1), program !! (pos+2), program !! (pos+3)]]
+            updatedProgram = updateProgram program (computeInstruction currentInstruction)
+            newPos = pos+4
+
+-- Process actionable instructions
+computeInstruction :: Instruction -> Result
+computeInstruction instruction 
+        | opcode instruction == Add = Result ((firstVal instruction)+(secondVal instruction)) (location instruction)
+        | opcode instruction == Multiply = Result ((firstVal instruction)*(secondVal instruction)) (location instruction)
 
 -- todo: deal nicely with End opcode 99
 parseInstruction :: [Int] -> Instruction
@@ -22,9 +46,9 @@ parseInstruction (opcode:firstVal:secondVal:location:rest)
 parseInstruction (opcode:rest) 
     | opcode == 99 = Instruction End 0 0 0
 
--- given a program, a location and a new value, return a new program with the value a the location updated
-updateProgram :: Program -> Int -> Int -> Program
-updateProgram program location newVal = replaceNth program location newVal
+-- given a program, and the result of an instruction, return a new program with the value a the location updated
+updateProgram :: Program -> Result -> Program
+updateProgram program result = replaceNth program (location result) (output result)
 
 replaceNth :: [Int] -> Int -> Int -> [Int]
 replaceNth _ _ [] = []
